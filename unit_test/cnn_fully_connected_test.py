799453d9172150a88c7a5ccd356e6fa90700af15
Raphael Fialho <rapguit@gmail.com>
2018-10-01 21:37:16 -0300

    Eliminar o conjunto de teste atual. Substituir por 20% do treinamento.

--- unit_test/cnn_fully_connected_test.py
+++ unit_test/cnn_fully_connected_test.py
@@ -1,8 +1,12 @@
-from os import path
+import matplotlib as mpl
+mpl.use('Agg')
+
 from numpy import *
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
+
+from sklearn.model_selection import train_test_split
 
 from tabulate import tabulate
 
@@ -39,7 +43,7 @@ def create_baseline_model():
      model.add(Dense(100, input_dim=10, activation='relu'))
      model.add(Dense(100, activation='relu'))
      model.add(Dense(1, activation='linear'))
-     model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae', 'mape', 'cosine'])
+     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
      return model
 
@@ -47,26 +51,24 @@ def create_baseline_model():
 if __name__ == '__main__':
 
     train_file = '../datasets/train.csv'
-    test_file = '../datasets/test.csv'
-
-    X_test = loadtxt(test_file, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), unpack=True, delimiter=',').T
-    Y_test = loadtxt(test_file, unpack=True, usecols=(11), delimiter=',')
 
     X = loadtxt(train_file, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), unpack=True, delimiter=',').T
     Y = loadtxt(train_file, unpack=True, usecols=(11), delimiter=',')
 
+    X_train, X_test, y_train, y_test = train_test_split(X, Y)
+
     print('Data loaded!')
 
     model = create_baseline_model()
-    hist = model.fit(X, Y, validation_data=(X_test, Y_test), epochs=1000)
+    hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
     preds = model.predict(X_test)
 
     pred = preds.reshape(len(preds))
-    real = Y_test
+    real = y_test
     t = tabulate(array([real, pred]).T[:50], headers=['Real', 'Predict'], tablefmt='orgtbl')
     print(t)
 
-    plot_scatter(X, Y, X_test, Y_test, preds)
+    plot_scatter(X_train, y_train, X_test, y_test, preds)
 
     plot(hist.history, 'mean_squared_error')
-    plot(hist.history, 'cosine_proximity')
+
