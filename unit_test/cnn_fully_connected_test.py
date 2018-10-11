38a25ae75f8f210195cdb9827038b8c068158f8a
raphael-fialho-mobicare <raphael.fialho@mobicare.com.br>
2018-10-11 16:07:20 -0300

    incluindo dropout

--- unit_test/cnn_fully_connected_test.py
+++ unit_test/cnn_fully_connected_test.py
@@ -2,21 +2,25 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 from numpy import *
+
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RandomizedSearchCV
+from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
 
 from modules.plotting.plot_service import *
 
 
 def create_baseline_model():
      model = Sequential()
-     model.add(Dense(250, input_dim=10, activation='relu'))
-     model.add(Dense(125, activation='relu'))
-     model.add(Dense(1, activation='linear'))
+     model.add(Dense(250, input_dim=10, kernel_initializer='normal', activation='relu'))
+     model.add(Dropout(0.2))
+     model.add(Dense(125, kernel_initializer='normal', activation='relu'))
+     model.add(Dropout(0.2))
+     model.add(Dense(1, kernel_initializer='normal', activation='linear'))
      model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
      return model
@@ -56,7 +60,7 @@ if __name__ == '__main__':
 
     print('Data loaded!')
 
-    model = KerasRegressor(build_fn = create_baseline_model())
+    model = KerasRegressor(build_fn = create_baseline_model)
     best_params = find_best_params(model, X_train, y_train)
 
     hist = model.fit(
@@ -75,5 +79,5 @@ if __name__ == '__main__':
     plot_table(real, pred)
     plot_scatter(X_train, y_train, X_val, y_val, X_test, y_test, preds)
     plot(hist.history, 'mean_squared_error')
-    plot_hm(real, preds.T[0])
+    plot_hm(real, pred)
 
