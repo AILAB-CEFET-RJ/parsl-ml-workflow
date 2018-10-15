8711af9ee90b8d2887c00e775a7e63f50aa94c37
Raphael Fialho <rapguit@gmail.com>
2018-10-15 12:47:32 -0300

    incluindo knn

---
+++ unit_test/knn_test.py
@@ -0,0 +1,65 @@
+import matplotlib as mpl
+mpl.use('Agg')
+
+from numpy import *
+from sklearn.model_selection import train_test_split
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import RandomizedSearchCV
+
+from modules.plotting.plot_service import *
+
+
+def find_best_params(X_train, y_train):
+    n_neighbors = [int(x) for x in linspace(start=3, stop=30, num=3)]
+
+    # create random grid
+    random_grid = {
+        'n_neighbors': n_neighbors
+    }
+
+    # Random search of parameters
+    search = RandomizedSearchCV(estimator=KNeighborsRegressor(), param_distributions=random_grid, scoring='neg_mean_squared_error', n_iter=100, cv=3, verbose=1, random_state=42, n_jobs=-1)
+    # Fit the model
+    search.fit(X_train, y_train)
+
+    # print results
+    print('Best Params:', search.best_params_)
+
+    return search.best_params_
+
+
+if __name__ == '__main__':
+
+    train_file = '../datasets/redshifts.csv'
+
+    X = loadtxt(train_file, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), unpack=True, delimiter=',').T
+    Y = loadtxt(train_file, unpack=True, usecols=(11), delimiter=',')
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+    print('Data loaded!')
+    plot_simple_table(X_train.T[:, :30])
+
+    best_params = find_best_params(X_train, y_train)
+
+    model = KNeighborsRegressor(n_neighbors = best_params['n_neighbors'])
+
+    hist = model.fit(X_train, y_train)
+    score = model.score(X_val, y_val)
+    preds = model.predict(X_test)
+
+    pred = preds.reshape(len(preds))
+    real = y_test
+
+    plot_scatter(X_train, y_train, X_val, y_val, X_test, y_test, preds)
+
+    # Compute the mean squared error of our predictions.
+    mse = (((pred - real) ** 2).sum()) / len(pred)
+
+    print('Cross-Val Score:', score)
+    print('Mean Squared Error:', mse)
+
+    plot_hm(real, pred)
+
