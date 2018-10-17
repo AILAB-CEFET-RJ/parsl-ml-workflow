f1c36c7c6b48392cd80b76d38fc9d067e8a0028a
raphael-fialho-mobicare <raphael.fialho@mobicare.com.br>
2018-10-17 18:39:45 -0300

    tentativa de plotar learning curve

--- modules/plotting/plot_losses_callback.py
+++ modules/plotting/plot_losses_callback.py
@@ -1,7 +1,11 @@
 import keras
 
-from IPython.display import clear_output
 from matplotlib import pyplot as plt
+
+from numpy import mean
+from numpy import std
+from numpy import shape
+from numpy import array
 
 
 class PlotLossesCallback(keras.callbacks.Callback):
@@ -18,23 +22,51 @@ class PlotLossesCallback(keras.callbacks.Callback):
         self.losses = []
         self.val_losses = []
 
-        self.fig = plt.figure()
+        self.stds = []
+        self.val_stds = []
+
+        self.fig = plt
+        self.fig.figure()
+        self.fig.grid()
+        self.fig.title("Learning Curves")
+
+        self.fig.xlabel("Training examples")
+        self.fig.ylabel("Score")
 
         self.logs = []
+
+    def on_train_end(self, logs={}):
+        self.stds = array(self.stds)
+        self.fig.fill_between(self.stds[:, 0], self.stds[:, 1], self.stds[:, 2], alpha=0.1, color="r")
+        self.fig.fill_between(self.val_stds[:, 0], self.val_stds[:, 1], self.val_stds[:, 2], alpha=0.1, color="g")
+
+        self.fig.plot(self.stds[:, 0], self.stds[:, 3], 'o-', color="r", label="Training score")
+        self.fig.plot(self.val_stds[:, 0], self.val_stds[:, 3], 'o-', color="g", label="Cross-validation score")
+
+        self.fig.legend(loc="best")
+
+        if self.__show_only:
+            self.fig.show()
+        else:
+            self.fig.savefig(self.__dir + 'learn_curves.png')
+
 
     def on_epoch_end(self, epoch, logs={}):
         self.logs.append(logs)
         self.x.append(self.i)
         self.losses.append(logs.get('loss'))
         self.val_losses.append(logs.get('val_loss'))
-        self.i += 1
 
-        clear_output(wait=True)
-        plt.plot(self.x, self.losses, label="loss")
-        plt.plot(self.x, self.val_losses, label="val_loss")
-        plt.legend()
+        if epoch > 0:
+            train_scores_mean = mean(self.losses)
+            train_scores_std = std(self.losses)
+            test_scores_mean = mean(self.val_losses)
+            test_scores_std = std(self.val_losses)
 
-        if self.__show_only:
-            plt.show()
-        else:
-            plt.savefig(self.__dir + 'losses.png')
+            shap = shape(self.losses)
+            self.stds.append([shap, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, train_scores_mean])
+
+            val_shap = shape(self.val_losses)
+            self.val_stds.append([val_shap, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, test_scores_mean])
+
+
