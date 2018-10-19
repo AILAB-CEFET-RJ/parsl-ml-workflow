05680bb7983322217f24b2bd1803ae252ced0b93
Raphael Fialho <rapguit@gmail.com>
2018-10-18 23:44:45 -0300

    ajustando os parametros do random search

--- unit_test/ann_fully_connected_test.py
+++ unit_test/ann_fully_connected_test.py
@@ -7,6 +7,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
+from keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
@@ -14,41 +15,43 @@ from sklearn.model_selection import RandomizedSearchCV
 from modules.plotting.plot_service import *
 
 
-def create_baseline_model(l1_units=300, l1_dp=0.1, l2_units=150, l2_dp=0.05):
+def create_baseline_model(l1_units=300, l1_dp=0.1, l2_units=150, l2_dp=0.05, lr=0.001):
      model = Sequential()
      model.add(Dense(l1_units, input_dim=10, kernel_initializer='normal', activation='relu'))
      model.add(Dropout(l1_dp))
      model.add(Dense(l2_units, kernel_initializer='normal', activation='relu'))
      model.add(Dropout(l2_dp))
      model.add(Dense(1, kernel_initializer='normal', activation='linear'))
-     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+
+     adam = Adam(lr=lr)
+     model.compile(loss='mse', optimizer=adam, metrics=['mse'])
 
      return model
 
 
-def create_model(l1_units=250, l1_dp=0.2, l2_units=125, l2_dp=0.2):
-    return KerasRegressor(build_fn=create_baseline_model, l1_units=l1_units, l1_dp=l1_dp, l2_units=l2_units, l2_dp=l2_dp, verbose=1)
+def create_model(l1_units=300, l1_dp=0.1, l2_units=150, l2_dp=0.05, lr=0.001 ):
+    return KerasRegressor(build_fn=create_baseline_model, l1_units=l1_units, l1_dp=l1_dp, l2_units=l2_units, l2_dp=l2_dp, lr=lr, verbose=1)
 
 
 def find_best_params(X_train, y_train):
-    batch_size = [int(x) for x in linspace(start=10, stop=100, num=10)]
-    epochs = [int(x) for x in linspace(start=50, stop=300, num=6)]
-    l1_units = [int(x) for x in linspace(start=200, stop=500, num=12)]
-    l1_dp = [0.1, 0.2, 0.3, 0,4, 0.6]
-    l2_units = [int(x) for x in linspace(start=100, stop=500, num=15)]
-    l2_dp = [0.1, 0.2, 0.3, 0,4, 0.6]
+    l1_units = random.randint(low=50, high=500, size=10)
+    l1_dp = [0.0, 0.1, 0.3, 0.6]
+
+    l2_units = random.randint(low=100, high=500, size=15)
+    l2_dp = [0.0, 0.1, 0.3, 0.6]
+
+    lr = [0.1, 0.001, 0.0001]
 
     params = dict(
-        batch_size = batch_size,
-        epochs = epochs,
         l1_units = l1_units,
         l1_dp = l1_dp,
         l2_units = l2_units,
-        l2_dp = l2_dp
+        l2_dp = l2_dp,
+        lr = lr
     )
 
     # Random search of parameters
-    search = RandomizedSearchCV(estimator=create_model(), param_distributions=params, scoring='neg_mean_squared_error', n_iter=100, cv=3, verbose=1, random_state=42, n_jobs=-1)
+    search = RandomizedSearchCV(estimator=create_model(), param_distributions=params, scoring='neg_mean_squared_error', n_iter=30, cv=3, verbose=1, random_state=42, n_jobs=-1)
 
     # Fit the model
     search.fit(X_train, y_train)
@@ -74,7 +77,7 @@ if __name__ == '__main__':
     plot_simple_table(X_train.T[:, :30])
 
     best_params = find_best_params(X_train, y_train)
-    model = create_model(best_params['l1_units'], best_params['l1_dp'], best_params['l2_units'], best_params['l2_dp'])
+    model = create_model(best_params['l1_units'], best_params['l1_dp'], best_params['l2_units'], best_params['l2_dp'], best_params['lr'])
 
     plot_losses = ann_plot_losses_callback()
     tensorboard = ann_tensorboard_callback()
@@ -84,8 +87,7 @@ if __name__ == '__main__':
         y_train,
         verbose=1,
         validation_data = (X_val, y_val),
-        epochs = best_params['epochs'],
-        batch_size = best_params['batch_size'],
+        epochs = 300,
         callbacks=[tensorboard, plot_losses]
     )
 
