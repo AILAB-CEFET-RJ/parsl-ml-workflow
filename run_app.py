9bd3424272585a572c866981e6ea19342d056716
raphael <rapguit@gmail.com>
2019-07-22 02:12:22 -0300

    atualiacao do caminho do dataset

--- run_app.py
+++ run_app.py
@@ -4,9 +4,7 @@ import pickle
 import datetime
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
+from modules.sampling.data_util import build_dataset
 
 from parsl.app.app import python_app
 from parsl.config import Config
@@ -18,24 +16,27 @@ from parsl.addresses import address_by_hostname
 
 #woker job
 @python_app(executors=["master_htex"])
-def runMaster(inputs={}, ks=[]):
-    from modules.model import runKNN
+def runMaster(inputs, hp):
+    from modules.model import runRForest
 
-    return runKNN(inputs, ks)
+    return runRForest(inputs, hp)
 
 #woker job
 @python_app(executors=["tauri_htex"])
-def runTauri(inputs={}, ks=[]):
-    from modules.model import runKNN
+def runTauri(inputs, hp):
+    from modules.model import runRForest
 
-    return runKNN(inputs, ks)
+    return runRForest(inputs, hp)
 
+    
 #woker job
 @python_app(executors=["adhafera_htex"])
-def runAdhafera(inputs, ks):
+def runAdhafera(inputs, hp):
     from modules.model import runKNN
+    from modules.model import runLinearRegretion
 
-    return runKNN(inputs, ks)
+    return runLinearRegretion(inputs, hp)
+
 
 #master job
 def run():
@@ -49,42 +50,44 @@ def run():
     parsl.load(executionConfig([tauri_hostname, adhafera_hostname], port, username, passwd, shared_dir))
     print("... config loaded!")
 
-    data = build_dataset()
-    data_b = pickle.dumps(data)
+    #data = build_dataset()
+    #data_b = pickle.dumps(data)
     print("... dataset loaded!")
 
     times = []
     for i in range(0, 3):
         ts = datetime.datetime.now()
 
-        master0 = runMaster(data_b, [*range(1, 3)])
-        master1 = runMaster(data_b, [*range(3, 5)])
-        master2 = runMaster(data_b, [*range(5, 7)])
-        master3 = runMaster(data_b, [*range(7, 9)])
-        master4 = runMaster(data_b, [*range(9, 11)])
+        #master0 = runMaster(data_b, [*np.arange(0.1, 0.11, 0.1)])
 
-        tauri0 = runTauri(data_b, [*range(11, 13)])
-        tauri1 = runTauri(data_b, [*range(13, 15)])
-        tauri2 = runTauri(data_b, [*range(15, 17)])
-        tauri3 = runTauri(data_b, [*range(17, 19)])
-        tauri4 = runTauri(data_b, [*range(19, 21)])
+        #master0 = runMaster(None, zip(np.random.randint(low=200, high=1000, size=5), np.random.randint(low=50, high=300, size=5)))
+        #master1 = runMaster(data_b, [*range(3, 5)])
+        #master2 = runMaster(data_b, [*range(5, 7)])
+        #master3 = runMaster(data_b, [*range(7, 9)])
+        #master4 = runMaster(data_b, [*range(9, 11)])
 
-        adh0 = runAdhafera(data_b, [*range(16, 23)])
-        adh1 = runAdhafera(data_b, [*range(23, 25)])
-        adh2 = runAdhafera(data_b, [*range(25, 27)])
-        adh3 = runAdhafera(data_b, [*range(27, 29)])
-        adh4 = runAdhafera(data_b, [*range(29, 31)])
+        tauri0 = runTauri(None, zip(np.random.randint(low=200, high=1000, size=10), np.random.randint(low=50, high=300, size=10)))
+        #tauri1 = runTauri(data_b, [*range(13, 15)])
+        #tauri2 = runTauri(data_b, [*range(15, 17)])
+        #tauri3 = runTauri(data_b, [*range(17, 19)])
+        #tauri4 = runTauri(data_b, [*range(19, 21)])
+
+        #adh0 = runAdhafera(data_b, [*np.arange(0.001, 0.01, 0.001)])
+        #adh1 = runAdhafera(data_b, [*range(23, 25)])
+        #adh2 = runAdhafera(data_b, [*range(25, 27)])
+        #adh3 = runAdhafera(data_b, [*range(27, 29)])
+        #adh4 = runAdhafera(data_b, [*range(29, 31)])
 
         print("Waiting results ...")
         outputs = [i.result() for i in [
-            master0, master1, master2, master3, master4,
-            tauri0, tauri1, tauri2, tauri3, tauri4,
-            adh0, adh1, adh2, adh3, adh4
+            #master0, #master1, master2, master3, master4,
+            tauri0, #tauri1, tauri2, tauri3, tauri4,
+            #adh0, #adh1, adh2, adh3, adh4
         ]]
 
-        print(outputs)
         ts = datetime.datetime.now() - ts
         times.append(ts)
+        print(outputs)
 
     print("times: " + str(times))
 
@@ -94,31 +97,6 @@ def run():
     std = np.std([t.total_seconds() for t in times])
     print("elapsed std: " + str(std))
 
-
-
-def build_dataset():
-    train_file = 'datasets/redshifts.csv'
-
-    x = np.loadtxt(train_file, usecols=(range(1, 6)), unpack=True, delimiter=',', dtype='float32').T
-    y = np.loadtxt(train_file, unpack=True, usecols=(11), delimiter=',', dtype='float32')
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
-
-    x_train = x_train.astype(np.float32)
-    x_test = x_test.astype(np.float32)
-    x_val = x_val.astype(np.float32)
-
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-    x_test = scaler.fit_transform(x_test)
-    x_val = scaler.fit_transform(x_val)
-
-    return {
-        "x_train": x_train, "y_train": y_train,
-        "x_test": x_test, "y_test": y_test,
-        "x_val": x_val, "y_val": y_val
-    }
 
 def executionConfig(hostname, port, username, passwd, shared_dir):
     cmd= ("/home/rfialho/.local/bin/process_worker_pool.py {debug} {max_workers} "
@@ -141,8 +119,8 @@ def executionConfig(hostname, port, username, passwd, shared_dir):
             HighThroughputExecutor(
                 label="tauri_htex",
                 cores_per_worker=1,
-                mem_per_worker=0.35,
-                max_workers=6,
+                mem_per_worker=1,
+                max_workers=1,
                 worker_debug=True,
                 working_dir= shared_dir,
                 worker_logdir_root=shared_dir,
