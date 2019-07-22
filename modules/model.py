e848066919a9056ae8efe883ed96dd568aae584d
raphael <rapguit@gmail.com>
2019-07-22 01:49:02 -0300

    atualixando modelo

--- modules/model.py
+++ modules/model.py
@@ -4,7 +4,10 @@ import pickle
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import SGDRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+from modules.sampling.data_util import build_dataset
 
 
 def runKNN(inputs, ks):
@@ -43,7 +46,11 @@ def runKNN(inputs, ks):
 
 
 def runLinearRegretion(inputs, lrs):
-    data = pickle.loads(inputs)
+    if inputs:
+        data = pickle.loads(inputs)
+    else:
+        data = build_dataset()
+
     outputs = []
 
     ts = datetime.datetime.now()
@@ -78,3 +85,48 @@ def runLinearRegretion(inputs, lrs):
         "msg": "Run LReg! [" + h + "] > elapsed time: " + str(ts),
         "data": outputs,
     }
+
+
+def runRForest(inputs, hp):
+    if inputs:
+        data = pickle.loads(inputs)
+    else:
+        data = build_dataset()
+
+    ts = datetime.datetime.now()
+
+    outputs = []
+    for n_estimator, max_depth in hp:
+        model = RandomForestRegressor(
+            n_estimators=n_estimator,
+            max_depth=max_depth,
+            random_state=42,
+            verbose=1,
+            n_jobs=-1
+        )
+
+        model.fit(data["x_train"], data["y_train"])
+
+        score = model.score(data["x_val"], data["y_val"])
+        preds = model.predict(data["x_test"])
+
+        pred = preds.reshape(len(preds))
+        real = data["y_test"]
+
+        mse = mean_squared_error(real, pred)
+
+        output = {}
+        output['score'] = score
+        output['mse'] = mse
+        output['hp'] = [n_estimator, max_depth]
+
+        outputs.append(output)
+
+    ts = datetime.datetime.now() - ts
+    h = socket.gethostname()
+
+    return {
+        "msg": "Run LReg! [" + h + "] > elapsed time: " + str(ts),
+        "data": outputs,
+    }
+
--- modules/sampling/data_util.py
+++ modules/sampling/data_util.py
@@ -1,6 +1,7 @@
+import numpy as np
 from numpy import loadtxt, savetxt
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
 
 def split_dataset(train_file):
     X = loadtxt(train_file, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), unpack=True, delimiter=',').T
@@ -13,7 +14,29 @@ def split_dataset(train_file):
 
     return X_train, y_train, X_test, y_test, X_val, y_val
 
+def build_dataset():
+    train_file = 'datasets/redshifts.csv'
 
+    x = np.loadtxt(train_file, usecols=(range(1, 6)), unpack=True, delimiter=',', dtype='float32').T
+    y = np.loadtxt(train_file, unpack=True, usecols=(11), delimiter=',', dtype='float32')
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+
+    x_train = x_train.astype(np.float32)
+    x_test = x_test.astype(np.float32)
+    x_val = x_val.astype(np.float32)
+
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.fit_transform(x_test)
+    x_val = scaler.fit_transform(x_val)
+
+    return {
+        "x_train": x_train, "y_train": y_train,
+        "x_test": x_test, "y_test": y_test,
+        "x_val": x_val, "y_val": y_val
+    }
 
 if __name__ == '__main__':
     train_file = '../../datasets/redshifts.csv'
